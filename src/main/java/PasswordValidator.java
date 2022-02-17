29e7fd73e164c56751111742c6e97e642b73b182
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-17 18:11:07 -0300

    feat: ensure PasswordValidator.validate returns string with error details if the password has no number provided

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -7,6 +7,11 @@ public class PasswordValidator {
             return Integer.toString(remainChars);
         }
 
+        boolean containNumber = hasNumber(password);
+        if (!containNumber) {
+            return "Error: Must contain at least 1 digit";
+        }
+
         return "success";
     }
 
@@ -14,7 +19,20 @@ public class PasswordValidator {
         int remainChars = 6 - password.length();
 
         return Math.max(remainChars, 0);
+    }
 
+    // https://www.tutorialspoint.com/Check-if-a-string-contains-a-number-using-Java
+    public static boolean hasNumber(String password) {
+        char[] chars = password.toCharArray();
+        boolean containNumber = false;
+
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                containNumber = true;
+            }
+        }
+
+        return containNumber;
     }
 
 }
