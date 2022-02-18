f1b79df7022deb722f83769a200b9fd4defaea30
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-18 15:16:35 -0300

    test: ensure CountAnagrams.count returns the number of anagrams found by single string

---
+++ src/test/java/CountAnagramsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CountAnagramsTest {
+    @Test
+    @DisplayName("Should count() returns the number of anagrams found by single string")
+    public void test() {
+        CountAnagrams SUT = new CountAnagrams();
+
+        String input = "ovo";
+        String input2 = "ifailuhkqq";
+
+        Assertions.assertEquals(SUT.count(input), 2);
+        Assertions.assertEquals(SUT.count(input2), 3);
+    }
+}
